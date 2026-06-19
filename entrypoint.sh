e2cce3f494047806b0d4e23eac05e9ed794119b3
Subhankar Roy Choudhury <subhankarrc1204@gmail.com>
2026-06-20 03:01:59 +0530

    Update code into server

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ if [[ -f "requirements.txt" ]]; then
   python -m pip install --upgrade pip
   python -m pip install -r requirements.txt
 fi
-python manage.py makemigrations --noinput
+# python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
 exec python manage.py runserver 0.0.0.0:8000
