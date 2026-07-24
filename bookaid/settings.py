3f56282b7fa5a0d5392b65136f3f928e22e2026c
Subhankar Roy Choudhury <subhankarrc1204@gmail.com>
2026-07-25 01:37:24 +0530

    changes on settings.py

--- bookaid/settings.py
+++ bookaid/settings.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 
 def load_secrets(secret_name=None, region="us-east-1"):
     if secret_name is None:
-        secret_name = get_env('secret_name')
+        secret_name = os.getenv('secret_name')
     client = boto3.client("secretsmanager", region_name=region)
     response = client.get_secret_value(SecretId=secret_name)
     return json.loads(response["SecretString"])
