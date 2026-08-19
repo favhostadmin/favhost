505b08bd431f7a86173954f389a0079bf4e8a363
abhishek-kumar <shadowatg00@gmail.com>
2026-08-19 20:26:44 +0530

    changes in the tiles of listings

--- property/views.py
+++ property/views.py
@@ -426,7 +426,7 @@ class PropertyListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         # queryset = Property.objects.prefetch_related('images', 'amenities').all()
-        queryset = Property.objects.filter(created_by__in=get_visible_user_ids(self.request.user)).prefetch_related('images').all()
+        queryset = Property.objects.filter(created_by__in=get_visible_user_ids(self.request.user)).prefetch_related('images', 'amenities').all()
         
         # Search functionality
         search_query = self.request.GET.get('search', '').strip()
