be02f4a7cad01463c18d437309669081c22bafb5
abhishek-kumar <shadowatg00@gmail.com>
2026-08-21 01:38:41 +0530

    fixed ui of listinf in mobile view

--- property/views.py
+++ property/views.py
@@ -894,6 +894,9 @@ class ListingPageView(ListView):
         else:
             return Property.objects.none()
 
+        # The cards show up to three amenity chips each.
+        queryset = queryset.prefetch_related('amenities')
+
         # Filter by availability if dates are provided
         if check_in and check_out:
             try:
